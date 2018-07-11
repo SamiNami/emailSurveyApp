80a87eb6a77d3f9fceaf9d420f0e780026e4d70e
sami peralahti <sami.peralahti@vainu.io>
2018-07-10 19:57:44 -0700

    added google Oath, needs configuring

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hello: 'world' });
-});
+
+// client id : 197004613402-8rromdh470li1bu9frvfp7h0pae85cnv.apps.googleusercontent.com
+// cleint secret: 5uAJ71YuPHqYJ8aJ31dX_nOP
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
