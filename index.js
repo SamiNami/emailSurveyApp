731851c1109a7f9742c4bc1cd2d2bb130773ff8f
sami peralahti <sami.peralahti@vainu.io>
2018-07-10 22:06:01 -0700

    fast solution to not pushing keys to github

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-
-// client id : 197004613402-8rromdh470li1bu9frvfp7h0pae85cnv.apps.googleusercontent.com
-// cleint secret: 5uAJ71YuPHqYJ8aJ31dX_nOP
 passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
