79b1b3faceac7e042a6e66aae4dee7a19d254c7c
sami peralahti <sami.peralahti@vainu.io>
2018-07-14 16:06:39 -0700

    basic setupd for redux

---
+++ client/src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+    return <div>Kappa2</div>;
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+
+// first arguement to create store is all the reducer inside our App
+// 2nd argument is the inital state of the app
+// 3rd argument is for applyMiddleware
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+);
