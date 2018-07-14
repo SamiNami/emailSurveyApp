7e7f492debd505b99f8cda594d0bdd58562301bc
sami peralahti <sami.peralahti@vainu.io>
2018-07-14 16:17:39 -0700

    redux boilerplate wired up

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,12 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
 // first arguement to create store is all the reducer inside our App
 // 2nd argument is the inital state of the app
 // 3rd argument is for applyMiddleware
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 ReactDOM.render(
     <Provider store={store}>
