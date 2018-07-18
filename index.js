034906b68663b1dece255a37dfdd2d1eb63f5464
sami peralahti <sami.peralahti@vainu.io>
2018-07-17 23:09:19 -0700

    removed non needed package

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-// const bodyParser = require('body-parser');  not safe to use here, remoe from node modules also...
 const keys = require('./config/keys');
 require('./models/user');
 require('./services/passport');
