f2fdd0b7b301364ab7ce272c9e03c12373d4f1ca
sami peralahti <sami.peralahti@vainu.io>
2018-08-01 14:07:05 -0700

    submit button, form values displayed in the review

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,14 +1,28 @@
 // surveyFromReviews shows user their form input for review
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = ({ onCancel, formValues }) => {
+    const reviewFields = formFields.map(({ name, label }) => {
+        return (
+            <div key={name}>
+                <label>{label}</label>
+                <div>{formValues[name]}</div>
+            </div>
+        );
+    });
+
     return (
         <div>
             <h5>Please confirm your entries</h5>
-
+            {reviewFields}
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
+            </button>
+            <button className="green btn-flat right" onClick={onCancel}>
+                Send Survey
+                <i className="right material-icons">email</i>
             </button>
         </div>
     );
