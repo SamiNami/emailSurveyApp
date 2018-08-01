1af70f5dddf36cb11b96cc4a97e1a6e5b1f709f6
sami peralahti <sami.peralahti@vainu.io>
2018-07-31 21:00:56 -0700

    created all the fields needed for the survery

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,10 @@
 // contains logic to render a single label and text input
 import React from 'react';
 
-export default ({ input }) => {
-    console.log(input);
+export default ({ input, label }) => {
     return (
         <div>
+            <label>{label}</label>
             <input {...input} />
         </div>
     );
