2f277aa4798ec6e9184517fadd6743cdbf4fa743
sami peralahti <sami.peralahti@vainu.io>
2018-08-01 14:50:41 -0700

    added a redirect to /surverys once a user submits a survey

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
 import * as actions from '../../actions';
+import { withRouter } from 'react-router-dom';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
     const reviewFields = formFields.map(({ name, label }) => {
         return (
             <div key={name}>
@@ -26,7 +27,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
             </button>
             <button
                 className="green btn-flat right white-text"
-                onClick={() => submitSurvey(formValues)}
+                onClick={() => submitSurvey(formValues, history)}
             >
                 Send Survey
                 <i className="right material-icons">email</i>
@@ -42,4 +43,4 @@ function mapStateToProps(state) {
 export default connect(
     mapStateToProps,
     actions
-)(SurveyFormReview);
+)(withRouter(SurveyFormReview));
