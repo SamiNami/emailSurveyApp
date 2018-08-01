4508391c50d5052d1475395f0de65f3560143b11
sami peralahti <sami.peralahti@vainu.io>
2018-08-01 14:21:38 -0700

    new action creator for the submit button

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -15,6 +15,12 @@ export const fetchUser = () => async dispatch => {
     dispatch({ type: FETCH_USER, payload: res.data });
 };
 
+export const submitSurvey = values => {
+    return {
+        type: 'submit_survey'
+    };
+};
+
 export const handleToken = token => async dispatch => {
     const res = await axios.post('/api/stripe', token);
 
--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
     const reviewFields = formFields.map(({ name, label }) => {
         return (
             <div key={name}>
@@ -17,10 +18,16 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
         <div>
             <h5>Please confirm your entries</h5>
             {reviewFields}
-            <button className="yellow darken-3 btn-flat white-text" onClick={onCancel}>
+            <button
+                className="yellow darken-3 btn-flat white-text"
+                onClick={onCancel}
+            >
                 Back
             </button>
-            <button className="green btn-flat right white-text" onClick={onCancel}>
+            <button
+                className="green btn-flat right white-text"
+                onClick={() => submitSurvey(formValues)}
+            >
                 Send Survey
                 <i className="right material-icons">email</i>
             </button>
@@ -32,4 +39,7 @@ function mapStateToProps(state) {
     return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(
+    mapStateToProps,
+    actions
+)(SurveyFormReview);
