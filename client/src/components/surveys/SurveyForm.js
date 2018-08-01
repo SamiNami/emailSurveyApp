095e8bdcb6f6fd548601706475c62b66418eb582
sami peralahti <sami.peralahti@vainu.io>
2018-07-31 21:16:54 -0700

    styling improvements

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
 
 const FIELDS = [
     { label: 'Survey Title', name: 'title' },
@@ -34,7 +35,16 @@ class SurveyForm extends Component {
                     )}
                 >
                     {this.renderFields()}
-                    <button type="submit">Submit</button>
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button
+                        type="submit"
+                        className="teal btn-flat right white-text"
+                    >
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
